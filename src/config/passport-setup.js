927e20fb67304468f6f00f51d2d83cbd31bee36d
Christopher <christopher.rozario7@gmail.com>
2018-10-25 17:53:22 +0530

    added keys

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -1,9 +1,11 @@
 import passport from 'passport';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+import keys from './keys';
 
 passport.use(
   new GoogleStrategy({
-  //options
+  clientID: keys.google.clientID,
+  clientSecret: keys.google.clientSecret
 }), () => {
   //callback function
 })
