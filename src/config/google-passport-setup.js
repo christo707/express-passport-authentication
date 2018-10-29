2086492b13159379a394c63c7e55dc026ed009cc
Christopher <christopher.rozario7@gmail.com>
2018-10-29 18:06:31 +0530

    Google

--- src/config/google-passport-setup.js
+++ src/config/google-passport-setup.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 import keys from './keys';
+import Account from '../model/account';
 
 passport.use(
   new GoogleStrategy({
@@ -10,4 +11,13 @@ passport.use(
 }, (accessToken, refreshToken, profile, done) => {
   console.log("Google Authentication completed");
   console.log(profile);
+  let account = new Account();
+  account.displayname = profile.displayName;
+  account.socialId = profile.id;
+  account.provider = 'google';
+  account.save().then((user) => {
+    console.log("New User Created: " + user);
+  }, (err) => {
+    console.log("Err in creating user" + err);
+  })
 }))
--- src/model/account.js
+++ src/model/account.js
@@ -2,17 +2,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 let Account = new Schema({
-  username: String,
+//  username: String,
+  diaplayname: String,
   socialId: String,
   provider: {
         type:String,
         required: true
     },
-  email: {
-        type:String,
-        required: true,
-        unique: true
-    }
+  // email: {
+  //       type:String,
+  //       required: true,
+  //       unique: true
+  //   }
 });
 
 module.exports = mongoose.model('Account', Account);
